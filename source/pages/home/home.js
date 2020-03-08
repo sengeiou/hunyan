ac334fc8c00f8f905a2587693770f9188d315cef
ss <992613643@qq.com>
2020-03-08 20:06:59 +0800

    qwe

---
+++ source/pages/home/home.js
@@ -0,0 +1,29 @@
+// pages/content/content.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+  }
+  onMyShow() {
+    var that = this;
+    var instapi = new InstApi();
+    instapi.indexbanner({}, (indexbanner) => {
+      this.Base.setMyData({
+        indexbanner
+      });
+    });
+  }
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+Page(body)
